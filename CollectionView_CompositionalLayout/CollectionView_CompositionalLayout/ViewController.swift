660c3c834ed7560cd82e0db93b3c605d9b15c91e
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2022-12-29 17:16:34 +0200

    Basic Collection View - Basic UI

--- CollectionView_CompositionalLayout/CollectionView_CompositionalLayout/ViewController.swift
+++ CollectionView_CompositionalLayout/CollectionView_CompositionalLayout/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let collectionView = UICollectionView(frame:.zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        
+            }
+    
+   
+
+
+}
+
+extension ViewController : UICollectionViewDataSource
+{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
     }
-
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as? MyCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
 }
 
